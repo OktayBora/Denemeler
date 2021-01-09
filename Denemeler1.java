7c700d8523f8907c0103a4f1f75b05681d8a1f2b
OktayBora <iletisim.bora@gmail.com>
2021-01-09 16:47:25 +0300

    Second java program

---
+++ Denemeler1.java
@@ -0,0 +1,17 @@
+public class Denemeler1 {
+
+ 
+	
+	public static void main(String[] args) {
+	
+		  int i;
+			System.out.println("Döngü başladı");
+			for (i=0; i<12; i++)
+			System.out.println("i= " + i);
+			System.out.println("Döngü Bitti");
+			
+			
+		
+	}
+}
+
